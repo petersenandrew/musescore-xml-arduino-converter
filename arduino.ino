b8f9cd28d6ea803659f6e7ea5cfee175681ed0f1
Andrew Petersen <180884956+petersenandrew@users.noreply.github.com>
2026-03-12 13:24:43 -0700

    add disc reading

--- arduino.ino
+++ arduino.ino
@@ -743,6 +743,8 @@ void checkRFID() {
   int8_t songIndex = findSongByRFID(mfrc522.uid.uidByte);
   
   if (songIndex >= 0 && songIndex < NUM_SONGS) {
+    lcd.clear();
+    lcd.print(F("Reading disc..."));
     // 2-second delay between detecting the disc and starting playback
     delay(2000);
     // Switch to this song (even if currently playing another)
